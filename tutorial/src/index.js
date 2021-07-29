5ad47af480983ad062121de30fb8704174eddc59
Sahil Khurana <sakhu@amazon.com>
2021-07-29 14:12:29 -0700

    Design components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,9 +1,41 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-//Greeting is a react component and it starts with capital letter
-function Greeting() {
-  return <h4>This is Sahil and this is my first Component</h4>;
+//CSS
+
+import "./index.css";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL640_FMwebp_QL65_.jpg"
+    alt=""
+  />
+);
+
+const Title = () => <h1> I Love you to the moon and back </h1>;
+
+const Author = () => (
+  <h4>Tim Warnes</h4>
+);
+ReactDom.render(<BookList />, document.getElementById("root"));
