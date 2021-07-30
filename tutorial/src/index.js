8fe6f6c592ca31d651b4664c39fdc9a5f415248d
Sahil Khurana <sahilk335@gmail.com>
2021-07-30 08:48:49 -0700

    props array

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,45 +3,37 @@ import ReactDom from "react-dom";
 
 //CSS
 import "./index.css";
-
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-  title: "I Love you to the moon and back",
-  author: "Tim Warnes",
-};
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
-  title: "Our Class is a Family",
-  author: "Shannon",
-};
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "I Love you to the moon and back",
+    author: "Tim Warnes",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
+    title: "Our Class is a family",
+    author: "Shannon",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71tqt4VL%2BdS._AC_UL200_SR200,200_.jpg",
+    title: "It Ends with Us: A Novel",
+    author: "Colleen Hoover",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
-        beatae, reiciendis alias harum sit necessitatibus sint! Omnis eos velit
-        adipisci.
-      </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book book={book}></Book>;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
   console.log(props);
-  const { img, title, author } = props;
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <img src={img} alt="" />
