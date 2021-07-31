e6f7c88a80afee5381fc07a90458cec57c9cb636
Sahil Khurana <sahilk335@gmail.com>
2021-07-31 16:38:56 -0700

    show hide component

--- react-advanced/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ react-advanced/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <h1></h1>
+      <button className="btn" onClick={() => setShow(!show)}>
+        Show/Hide
+      </button>
+      {show && <ShowAnotherComponent />}
+    </React.Fragment>
+  );
+};
+
+const ShowAnotherComponent = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello World</h1>
+    </React.Fragment>
+  );
 };
 
 export default ShowHide;
