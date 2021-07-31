8cf75ba5f7cef5e3de60f17b4530d8e2232442c7
Sahil Khurana <sahilk335@gmail.com>
2021-07-31 12:15:22 -0700

    useEffects Basics

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/5-useState-counter'
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
+
 function App() {
   return (
     <div className='container'>
--- react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = React.useState(0);
+  React.useEffect(() => {
+    if(value>=2){
+      document.title=`New Message (${value})`;
+    }
+    console.log("Hello World");
+  }, [value]);
+
+  React.useEffect(()=>{
+    console.log("Called Every time on rendering");
+  })
+
+    React.useEffect(() => {
+      console.log("Called once on rendering");
+    },[]);
+
+
+  return (
+    <React.Fragment>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Increment
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectBasics;
